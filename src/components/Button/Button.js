b81017f11add550b08aa277efa4e2533881cf17e
Ivan Dubovtsov <followhappyq@gmail.com>
2020-04-14 21:45:28 +0200

    login form added

---
+++ src/components/Button/Button.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button as BaseButton } from "antd";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+import "./Button.scss";
+
+const Button = (props) => {
+  return (
+    <BaseButton
+      className={classNames("button", props.className, {
+        "button--large": props.size === "large",
+      })}
+      {...props}
+    />
+  );
+};
+
+Button.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Button;
