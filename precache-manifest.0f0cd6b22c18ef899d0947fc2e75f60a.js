35cfa703438feb17dd986a85858c1177cd187e5e
Ivan <followhappyq@gmail.com>
2020-05-01 11:02:10 +0200

    Updates

---
+++ precache-manifest.0f0cd6b22c18ef899d0947fc2e75f60a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2020308cd86d31067b2b109c4d1af6bc",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "2599677e5c5aaa5ebe93",
+    "url": "/messenger/static/css/2.4a42413a.chunk.css"
+  },
+  {
+    "revision": "20e33af1d6e49de98c16",
+    "url": "/messenger/static/css/main.5974b93c.chunk.css"
+  },
+  {
+    "revision": "2599677e5c5aaa5ebe93",
+    "url": "/messenger/static/js/2.6a13ff57.chunk.js"
+  },
+  {
+    "revision": "1a2f2540a78b2022a70889f2de7372f4",
+    "url": "/messenger/static/js/2.6a13ff57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e33af1d6e49de98c16",
+    "url": "/messenger/static/js/main.b42ffd09.chunk.js"
+  },
+  {
+    "revision": "958d70a3d077ef66cf79",
+    "url": "/messenger/static/js/runtime-main.905fe952.js"
+  },
+  {
+    "revision": "1456f01338efe100733984d48807dd08",
+    "url": "/messenger/static/media/noreaded.1456f013.svg"
+  },
+  {
+    "revision": "2da46147a19449255266514c196ca9cc",
+    "url": "/messenger/static/media/readed.2da46147.svg"
+  }
+]);
